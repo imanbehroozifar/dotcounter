05de1a5d93d68e87e671bb48e2a43a147e2defac
Iman Behroozifar <imanbehroozifar78@gmail.com>
2022-07-30 10:12:45 +0430

    Completion of js codes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const  App = ()=> {
+  const [state , setState] = useState({
+    value: '',
+    count: 0,
+  })
+  const handleValue = (event) => {
+    setState({...state, value:event.target.value })
+
+  }
+  const handleNote = () => {
+    const value = state.value,
+      oneDot = ['ب', 'ج', 'خ', 'ذ', 'ز', 'ض', 'ظ', 'غ', 'ن'],       
+      twoDot = ['ت', 'ق'],
+      terdDot = ['پ', 'ث', 'چ', 'ش'],
+      exception = ['ی']
+
+    
+    let text = value.split('')
+
+    let dots = 0;
+    for (let i = 0; i < text.length; i++) {
+      let letters = text[i]
+      if (oneDot.includes(letters)) {
+        dots++
+      }
+      if (twoDot.includes(letters)) {
+        dots += 2
+      }
+      if (terdDot.includes(letters)) {
+        dots += 3
+      }
+      if (exception.includes(letters) && i !== text.length - 1) {
+        if (text[i + 1] !== ' ') {
+          dots += 2
+        }
+      }
+      
+      
+      setState({...state,count:dots,value:''})
+    }
+
+  }
   return (
     <div className="App">
-      <input type="text" className="input" />
-      <button className="btn">count</button>
-      <span className="span">0</span>
+      <input type="text" className="input" value={state.value} onChange={handleValue}/>
+      <button onClick={handleNote} className="btn">count</button>
+      <span className="span">{state.count}</span>
     </div>
   );
 }
